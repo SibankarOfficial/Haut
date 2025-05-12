d10136520ede7c9a312c881f44011ced928d41cb
SibankarOfficial <159782127+SibankarOfficial@users.noreply.github.com>
2025-05-12 22:35:17 +0530

    done

--- js/main.js
+++ js/main.js
@@ -321,3 +321,23 @@ window.addEventListener('load', function () {
     document.querySelector('.main-content').style.display = 'block';
   }, 2000); // 2 seconds
 });
+
+AOS.init();
+
+   // tabs
+        // --------------------------------------------------
+        jQuery('.de_tab').find('.de_tab_content > div').hide();
+        jQuery('.de_tab').find('.de_tab_content > div:first').show();
+        jQuery('li').find('.v-border').fadeTo(150, 0);
+        jQuery('li.active').find('.v-border').fadeTo(150, 1);
+
+        jQuery('.de_nav li').click(function () {
+            jQuery(this).parent().find('li').removeClass("active");
+            jQuery(this).addClass("active");
+            jQuery(this).parent().parent().find('.v-border').fadeTo(150, 0);
+            jQuery(this).parent().parent().find('.de_tab_content > div').hide();
+
+            var indexer = jQuery(this).index(); //gets the current index of (this) which is #nav li
+            jQuery(this).parent().parent().find('.de_tab_content > div:eq(' + indexer + ')').fadeIn(); //uses whatever index the link has to open the corresponding box 
+            jQuery(this).find('.v-border').fadeTo(150, 1);
+        });
